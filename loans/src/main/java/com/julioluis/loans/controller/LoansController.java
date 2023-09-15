9aaea65f2351d85a062378425804001066ba67c9
jpelegrino1010 <julio.pelegrino@viseo.com>
2023-09-15 07:47:17 -0400

    update loans configserver

--- loans/src/main/java/com/julioluis/loans/controller/LoansController.java
+++ loans/src/main/java/com/julioluis/loans/controller/LoansController.java
@@ -4,10 +4,16 @@
 package com.julioluis.loans.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.julioluis.loans.config.AccountServerConfig;
 import com.julioluis.loans.model.Customer;
 import com.julioluis.loans.model.Loans;
+import com.julioluis.loans.model.Properties;
 import com.julioluis.loans.repository.LoansRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +31,9 @@ public class LoansController {
 	@Autowired
 	private LoansRepository loansRepository;
 
+	@Autowired
+	private AccountServerConfig accountServerConfig;
+
 	@PostMapping("/myLoans")
 	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
 		List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
@@ -34,6 +43,15 @@ public class LoansController {
 			return null;
 		}
 
+	}
+
+	@GetMapping("/loans/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(accountServerConfig.getMsg(), accountServerConfig.getBuildVersion(),
+				accountServerConfig.getMailDetails(), accountServerConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
 	}
 
 }
