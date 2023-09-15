b63d3cb44cd591c0edf0583a233d9fbb747310e1
jpelegrino1010 <julio.pelegrino@viseo.com>
2023-09-15 07:55:30 -0400

    update card configserver

---
+++ cards/src/main/java/com/julioluis/cards/config/CardServerConfig.java
@@ -0,0 +1,21 @@
+package com.julioluis.cards.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@ConfigurationProperties(prefix = "cards")
+@Getter @Setter @ToString
+public class CardServerConfig {
+
+    private String msg;
+    private String buildVersion;
+    private Map<String,String> mailDetails;
+    private List<String> activeBranches;
+}
--- cards/src/main/java/com/julioluis/cards/controller/CardsController.java
+++ cards/src/main/java/com/julioluis/cards/controller/CardsController.java
@@ -3,10 +3,16 @@
  */
 package com.julioluis.cards.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.julioluis.cards.config.CardServerConfig;
 import com.julioluis.cards.model.Cards;
 import com.julioluis.cards.model.Customer;
+import com.julioluis.cards.model.Properties;
 import com.julioluis.cards.repository.CardsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +30,9 @@ public class CardsController {
 	@Autowired
 	private CardsRepository cardsRepository;
 
+	@Autowired
+	private CardServerConfig cardServerConfig;
+
 	@PostMapping("/myCards")
 	public List<Cards> getCardDetails(@RequestBody Customer customer) {
 		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
@@ -33,6 +42,15 @@ public class CardsController {
 			return null;
 		}
 
+	}
+
+	@GetMapping("/cards/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(cardServerConfig.getMsg(), cardServerConfig.getBuildVersion(),
+				cardServerConfig.getMailDetails(), cardServerConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
 	}
 
 }
