84ab5b7244f3f691fe9b521f71907bca2e8b2ce5
jpelegrino1010 <julio.pelegrino@viseo.com>
2023-09-15 07:31:15 -0400

    update configserver for accounts

--- accounts/src/main/java/com/julioluis/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/julioluis/accounts/controller/AccountsController.java
@@ -4,10 +4,16 @@
 package com.julioluis.accounts.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.julioluis.accounts.config.AccountServerConfig;
 import com.julioluis.accounts.model.Accounts;
 import com.julioluis.accounts.model.Customer;
+import com.julioluis.accounts.model.Properties;
 import com.julioluis.accounts.repository.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +29,9 @@ public class AccountsController {
 	@Autowired
 	private AccountsRepository accountsRepository;
 
+	@Autowired
+	private AccountServerConfig accountServerConfig;
+
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
 
@@ -33,6 +42,15 @@ public class AccountsController {
 			return null;
 		}
 
+	}
+
+	@GetMapping("/account/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(accountServerConfig.getMsg(), accountServerConfig.getBuildVersion(),
+				accountServerConfig.getMailDetails(), accountServerConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
 	}
 
 }
